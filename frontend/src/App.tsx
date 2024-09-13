b60c83db62134fc0cb34a674ce149a1db48f6eb0
carvalhaus <ooliveira.joaop@gmail.com>
2024-09-13 19:20:51 -0300

    fix: change subtitle

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,8 +5,8 @@ function App() {
     <main>
       <h1>Medidor de consumo</h1>
       <h2>
-        Você só precisa tirar uma foto do seu medidor de água ou gás e, com o
-        auxílio da IA, realizaremos a leitura automaticamente.
+        Tire uma foto do seu medidor de água ou gás e, com o auxílio da
+        Inteligência Artificial, realizaremos a leitura automaticamente.
       </h2>
 
       <MeasureForm />
