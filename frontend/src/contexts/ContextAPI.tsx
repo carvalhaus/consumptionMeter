0bba81cfac20e60a2f567f0e7c7e5be9e5d5d093
carvalhaus <ooliveira.joaop@gmail.com>
2024-09-14 12:51:15 -0300

    feat: create ContextAPI and interfaces

---
+++ frontend/src/contexts/ContextAPI.tsx
@@ -0,0 +1,33 @@
+import { createContext, ReactNode, useContext } from "react";
+import useMeasurements from "../hooks/useMeasurements";
+import { IContextAPI } from "../interfaces/context";
+
+const ContextAPI = createContext<IContextAPI | undefined>(undefined);
+
+export function useApi(): IContextAPI {
+  const context = useContext(ContextAPI);
+
+  if (!context) {
+    throw new Error("useApi must be used within a ContextAPIProvider");
+  }
+
+  return context;
+}
+
+interface ContextAPIProviderProps {
+  children: ReactNode;
+}
+
+function ContextAPIProvider({ children }: ContextAPIProviderProps) {
+  const { measurements, loading, error, getMeasurements } = useMeasurements();
+
+  return (
+    <ContextAPI.Provider
+      value={{ measurements, loading, error, getMeasurements }}
+    >
+      {children}
+    </ContextAPI.Provider>
+  );
+}
+
+export default ContextAPIProvider;
---
+++ frontend/src/interfaces/context.ts
@@ -0,0 +1,8 @@
+import { IMeasurementsParams, IMeasurementsResponse } from "./measurements";
+
+export interface IContextAPI {
+  measurements: IMeasurementsResponse | null;
+  loading: boolean;
+  error: string | null;
+  getMeasurements: (params: IMeasurementsParams) => Promise<void>;
+}
